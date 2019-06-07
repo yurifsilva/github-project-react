48ba47b9e005090ce2bdfa56ac2d311847a25d53
Yuri F <xd_yuri@hotmail.com>
2019-06-06 21:35:56 -0300

    init redux

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/css/App.css';
+import { Provider } from 'react-redux';
+import store from '../store';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
